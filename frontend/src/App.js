ccc12f7ce4da3f341ccf8239d112f7677d6157f9
CleverMNL <clevernieto556@gmail.com>
2025-02-22 20:59:46 -0600

    mde changes to app file to include more html pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,54 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/conversation" element={<ConversationPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
       </Routes>
     </Router>
   );
 }
+
+
+function ConversationPage() {
+  return (
+    <iframe
+      src="/home_polytalk.html"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+      title="Conversation Page"
+    ></iframe>
+  );
+}
+
+function ConversationPage() {
+  return (
+    <iframe
+      src="/home_polytalk.html"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+      title="Conversation Page"
+    ></iframe>
+  );
+}
+
+function LoginPage() {
+  return (
+    <iframe
+      src="/login.html"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+      title="Login Page"
+    ></iframe>
+  );
+}
+
+function OnboardingPage() {
+  return (
+    <iframe
+      src="/onboarding.html"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+      title="Onboarding Page"
+    ></iframe>
+  );
+}
+
 
 export default App;
