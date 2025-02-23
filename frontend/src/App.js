e1ec0361923d9512819f699357e445baa9e5e17c
CleverMNL <clevernieto556@gmail.com>
2025-02-23 09:53:54 -0600

    removed no ModulesInHierachicDirectoriesPlugin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+import '../styles/styles.css';
 
 function App() {
   return (
@@ -8,54 +9,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/conversation" element={<ConversationPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/onboarding" element={<OnboardingPage />} />
       </Routes>
     </Router>
   );
 }
-
-
-function ConversationPage() {
-  return (
-    <iframe
-      src="/home_polytalk.html"
-      style={{ width: "100%", height: "100vh", border: "none" }}
-      title="Conversation Page"
-    ></iframe>
-  );
-}
-
-function ConversationPage() {
-  return (
-    <iframe
-      src="/home_polytalk.html"
-      style={{ width: "100%", height: "100vh", border: "none" }}
-      title="Conversation Page"
-    ></iframe>
-  );
-}
-
-function LoginPage() {
-  return (
-    <iframe
-      src="/login.html"
-      style={{ width: "100%", height: "100vh", border: "none" }}
-      title="Login Page"
-    ></iframe>
-  );
-}
-
-function OnboardingPage() {
-  return (
-    <iframe
-      src="/onboarding.html"
-      style={{ width: "100%", height: "100vh", border: "none" }}
-      title="Onboarding Page"
-    ></iframe>
-  );
-}
-
 
 export default App;
